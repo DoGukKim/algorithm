86c1b7bef83344a934c37c83840e21a7a3a4c4ae
김도국 <dgkim3811@naver.com>
2022-05-24 09:52:25 +0900

    upper fibonacci

--- book/recursion/fibonacci.js
+++ book/recursion/fibonacci.js
@@ -1,4 +1,4 @@
-// 일방적인 피보나치 풀이
+// 일방적인 재귀를 활용한 피보나치 풀이
 // O(N^2)
 const solution = (n) => {
   if (n === 0 || n === 1) return n;
@@ -13,4 +13,22 @@ const memoSolution = (n, memo = new Map()) => {
     memo.set(n, solution(n - 2, memo) + solution(n - 1, memo));
   }
   return memo.get(n);
+};
+
+// 상향식 피보나치 풀이
+// O(N)
+const upperSolution = (n) => {
+  if (n === 0) return 0;
+
+  let n1 = 0;
+  let n2 = 1;
+  let temp;
+
+  for (let i = 1; i < n; i++) {
+    temp = n1;
+    n1 = n2;
+    n2 = temp + n1;
+  }
+
+  return n2;
 };
