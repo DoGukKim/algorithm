baf42bb054edf3dcc1ce5291c41ff573c90a6b69
김도국 <dgkim3811@naver.com>
2022-05-24 09:38:19 +0900

    memo recursion

--- book/recursion/fibonacci.js
+++ book/recursion/fibonacci.js
@@ -1,5 +1,16 @@
+// 일방적인 피보나치 풀이
+// O(N^2)
 const solution = (n) => {
-  if (n < 2) return 1;
-  return solution(n - 1) + solution(n - 2);
+  if (n === 0 || n === 1) return n;
+  return solution(n - 2) + solution(n - 1);
 };
-solution(7);
+
+// 메모이제이션을 통한 피보나치 풀이
+// O(N)
+const memoSolution = (n, memo = new Map()) => {
+  if (n === 0 || n === 1) return n;
+  if (!memo.has(n)) {
+    memo.set(n, solution(n - 2, memo) + solution(n - 1, memo));
+  }
+  return memo.get(n);
+};
