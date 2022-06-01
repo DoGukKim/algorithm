1077c2131ad64677ed82b678ef4e0d9fdf2d5e84
김도국 <dgkim3811@naver.com>
2022-06-01 14:53:16 +0900

    봉우리의 합 문제 복습

--- inflearn/basic/etc.js
+++ inflearn/basic/etc.js
@@ -1,4 +1,3 @@
-// 가장 긴 문자열 도출
 const findLongStr = (arr) => {
   let result = arr[0];
   for (let i = 0; i < arr.length; i++) {
@@ -15,3 +14,25 @@ const deleteDuplicatedStr = (s) => {
   }
   return result;
 };
+
+const 봉우리의합 = (arr) => {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      const cur = arr[i][j];
+      let top = arr[i - 1] ? arr[i - 1][j] : 0;
+      let bottom = arr[i + 1] ? arr[i + 1][j] : 0;
+      let right = arr[i][j + 1] ? arr[i][j + 1] : 0;
+      let left = arr[i][j - 1] ? arr[i][j - 1] : 0;
+      if (cur > top && cur > bottom && cur > right && cur > left) result++;
+    }
+  }
+  return result;
+};
+const result = 봉우리의합([
+  [5, 3, 7, 2, 3],
+  [3, 7, 1, 6, 1],
+  [7, 2, 5, 3, 4],
+  [4, 3, 6, 4, 1],
+  [8, 7, 3, 5, 2],
+]);
