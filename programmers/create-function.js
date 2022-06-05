0cc978e128bddd0da27c0b0366504b73aadd2b49
김도국 <dgkim3811@naver.com>
2022-06-05 13:48:41 +0900

    programers test

--- programmers/create-function.js
+++ programmers/create-function.js
@@ -1,3 +1,4 @@
+// 풀이
 const solution = (p, s) => {
   const answer = [];
   const queue = [];
@@ -29,3 +30,23 @@ const solution = (p, s) => {
 
 const result = solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]);
 console.log(result);
+
+// 다른 사람 풀이
+function solution(progresses, speeds) {
+  let answer = [0];
+  let days = progresses.map((progress, index) =>
+    Math.ceil((100 - progress) / speeds[index])
+  );
+  let maxDay = days[0];
+
+  for (let i = 0, j = 0; i < days.length; i++) {
+    if (days[i] <= maxDay) {
+      answer[j] += 1;
+    } else {
+      maxDay = days[i];
+      answer[++j] = 1;
+    }
+  }
+
+  return answer;
+}
