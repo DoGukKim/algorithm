0675d0fafa9084ee671a522a1b4956cca60c5797
DoGukKim <dgkim3811@naver.com>
2022-06-09 14:12:03 +0900

    meeting room test

--- inflearn/sort/meetingroom.js
+++ inflearn/sort/meetingroom.js
@@ -1,3 +1,4 @@
+// 풀었으나 반례 사례 발견으로 오답.
 const solution = (arr) => {
   let answer = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -9,29 +10,27 @@ const solution = (arr) => {
         count++;
       }
     }
-
     if (count > answer) answer = count;
   }
-
   return answer;
 };
 
 const result = solution([
-  [3, 3],
-  [1, 3],
+  [1, 4],
   [2, 3],
+  [3, 5],
+  [4, 6],
+  [5, 7],
 ]);
 
+// 반례 - 끝나는 시간이 같음
 // const result = solution([
-//   [1, 4],
-//   [2, 3],
-//   [3, 5],
-//   [4, 6],
-//   [5, 7],
-// ]);
+//     [3, 3],
+//     [1, 3],
+//     [2, 3],
+//   ]);
 
 // 다른 사람 풀이
-
 function solution(meeting) {
   let answer = 0;
   meeting.sort((a, b) => {
@@ -47,3 +46,23 @@ function solution(meeting) {
   }
   return answer;
 }
+
+// 재풀이
+const solution = (arr) => {
+  let result = 0;
+  arr.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
+
+  let endPoint = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] >= endPoint) {
+      result++;
+      endPoint = arr[i][1];
+    }
+  }
+
+  return result;
+};
