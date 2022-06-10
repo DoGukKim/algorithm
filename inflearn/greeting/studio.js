8f4efb7639257a822461916ff4035117c7dcbb8c
DoGukKim <dgkim3811@naver.com>
2022-06-10 12:44:21 +0900

    greeting test

---
+++ inflearn/greeting/studio.js
@@ -0,0 +1,28 @@
+const solution = (arr, m) => {
+  let result = 0;
+  let startPoint = Math.max(...arr);
+  let endPoint = arr.reduce((acc, cur) => acc + cur, 0);
+
+  while (startPoint <= endPoint) {
+    const middlePoint = Math.floor((startPoint + endPoint) / 2);
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (sum + arr[i] > middlePoint) {
+        result++;
+        sum = arr[i];
+      } else {
+        sum += arr[i];
+      }
+    }
+
+    if (result <= m) {
+      endPoint = middlePoint - 1;
+    } else {
+      startPoint = middlePoint + 1;
+    }
+  }
+
+  return result;
+};
+const result = solution([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
