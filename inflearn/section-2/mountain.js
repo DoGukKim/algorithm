32e9be56109d7cc08d4020d087e865e449e3706e
DoGukKim <dgkim3811@naver.com>
2022-06-19 09:40:47 +0900

    repeat

--- inflearn/section-2/mountain.js
+++ inflearn/section-2/mountain.js
@@ -1,22 +1,26 @@
-const mountain = (arr) => {
+const solution = (arr) => {
   let result = 0;
+
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       const cur = arr[i][j];
       let top = arr[i - 1] ? arr[i - 1][j] : 0;
-      let bottom = arr[i + 1] ? arr[i + 1][j] : 0;
       let right = arr[i][j + 1] ? arr[i][j + 1] : 0;
+      let bottom = arr[i + 1] ? arr[i + 1][j] : 0;
       let left = arr[i][j - 1] ? arr[i][j - 1] : 0;
-      if (cur > top && cur > bottom && cur > right && cur > left) result++;
+
+      if (cur > top && cur > right && cur > bottom && cur > left) result++;
     }
   }
+
   return result;
 };
 
-mountain([
+const result = solution([
   [5, 3, 7, 2, 3],
   [3, 7, 1, 6, 1],
   [7, 2, 5, 3, 4],
   [4, 3, 6, 4, 1],
   [8, 7, 3, 5, 2],
 ]);
+console.log(result);
