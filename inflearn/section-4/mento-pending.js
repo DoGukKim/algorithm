0755a2a747b6ca062cb75716200869ea56aa7eaf
DoGukKim <dgkim3811@naver.com>
2022-06-28 23:30:35 +0900

    repeat section 4

--- inflearn/section-4/mento-pending.js
+++ inflearn/section-4/mento-pending.js
@@ -1,10 +1,19 @@
-const main = (n, m, arr) => {
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {}
+const main = (n, arr) => {
+  const students = Array.from({ length: n }, (_, i) => i + 1);
+
+  let result = 0;
+  for (let i = 0; i < students.length; i++) {
+    let minRank = 0;
+    for (let j = 0; j < arr.length; j++) {
+      const rank = arr[j].findIndex((item) => item === students[i]);
+      if (rank > minRank) minRank = rank;
+    }
+    result += students.length - 1 - minRank;
   }
+  console.log(result);
 };
 
-main(4, 3, [
+main(4, [
   [3, 4, 1, 2],
   [4, 3, 2, 1],
   [3, 1, 4, 2],
--- inflearn/section-4/target-k-pending.js
+++ inflearn/section-4/target-k-pending.js
@@ -1,5 +1,19 @@
-const solution = (k, arr) => {
-  for (let i = 0; i < arr.length; i++) {}
+const main = (k, arr) => {
+  let result = [];
+  const hash = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let t = j + 1; t < arr.length; t++) {
+        const sum = arr[i] + arr[j] + arr[t];
+        if (!hash[sum]) {
+          hash[sum] = true;
+          result.push(sum);
+        }
+      }
+    }
+  }
+
+  console.log(result.sort((a, b) => b - a)[k - 1]);
 };
-const result = solution(3, [13, 15, 34, 23, 45, 65, 33, 11, 26, 42]);
-console.log(result);
+main(3, [13, 15, 34, 23, 45, 65, 33, 11, 26, 42]);
