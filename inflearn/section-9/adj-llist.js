3eaab203a79ebf022fb3e4cc1c2fc4689d865b2e
DoGukKim <dgkim3811@naver.com>
2022-07-04 08:38:45 +0900

    graph test

---
+++ inflearn/section-9/adj-llist.js
@@ -0,0 +1,40 @@
+const main = (n, arr) => {
+  const graph = Array.from({ length: n + 1 }, () => Array());
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let result = 0;
+  let path = [];
+  for (const [a, b] of arr) {
+    graph[a].push(b);
+  }
+  function DFS(v) {
+    if (v === n) {
+      result++;
+      console.log(path);
+    } else {
+      for (let nv of graph[v]) {
+        if (ch[nv] === 0) {
+          path.push(nv);
+          ch[nv] = 1;
+          DFS(nv);
+          ch[nv] = 0;
+          path.pop();
+        }
+      }
+    }
+  }
+  ch[1] = 1;
+  path.push(1);
+  DFS(1);
+  console.log(result);
+};
+main(5, [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+]);
