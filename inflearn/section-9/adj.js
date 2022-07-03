724d97270c2d5e378062e715d30e185c0c3a8782
DoGukKim <dgkim3811@naver.com>
2022-07-03 19:29:51 +0900

    section 9

--- inflearn/section-9/adj.js
+++ inflearn/section-9/adj.js
@@ -1,12 +1,34 @@
-const main = (n, arr) => {
+const main = (n, edges) => {
   const graph = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
-  const ch = Array.from({ length: n + 1 }, () => 0);
-  for (const [a, b] of arr) {
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  for (const edge of edges) {
+    const [a, b] = edge;
     graph[a][b] = 1;
   }
-
-  console.log(graph);
+  let result = 0;
+  const path = [];
+  const explore = (v) => {
+    if (v === n) {
+      result++;
+      console.log(path);
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (graph[v][i] === 1 && ch[i] === 0) {
+          ch[i] = 1;
+          path.push(i);
+          explore(i);
+          ch[i] = 0;
+          path.pop();
+        }
+      }
+    }
+  };
+  path.push(1);
+  ch[1] = 1;
+  explore(1);
+  console.log(result);
 };
+
 main(5, [
   [1, 2],
   [1, 3],
