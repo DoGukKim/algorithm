753d8f316d3a8e25b2bb6d78de87773ef57359f5
DoGukKim <dgkim3811@naver.com>
2022-07-07 14:17:09 +0900

    section 10 coin

--- inflearn/section-10/coins.js
+++ inflearn/section-10/coins.js
@@ -5,7 +5,8 @@
 // Memorize
 // TimeComplexity O(n)
 // SpaceComplexity O(n)
-const bestSumCases = (money, coins) => {
+const bestSumCases = (money, coins, memo = {}) => {
+  if (money in memo) return memo[money];
   if (money === 0) return [];
   if (money < 0) return null;
 
@@ -13,7 +14,7 @@ const bestSumCases = (money, coins) => {
 
   for (let i = 0; i < coins.length; i++) {
     const remainder = money - coins[i];
-    const remainderResult = bestSumCases(remainder, coins);
+    const remainderResult = bestSumCases(remainder, coins, memo);
     if (remainderResult !== null) {
       const combinationResult = [...remainderResult, coins[i]];
       if (
@@ -25,6 +26,21 @@ const bestSumCases = (money, coins) => {
     }
   }
 
+  memo[money] = shortestCombination;
   return shortestCombination;
 };
 console.log(bestSumCases(15, [1, 2, 5]));
+
+const main = (m, arr) => {
+  if (m === 0) return 1;
+  if (m < 0) return 0;
+
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const remainder = m - arr[i];
+    count += main(remainder, arr);
+  }
+
+  return count;
+};
--- inflearn/section-10/stair.js
+++ inflearn/section-10/stair.js
@@ -16,4 +16,14 @@ const numberOfCases = (c, memo = {}) => {
 
   return result;
 };
-console.log(numberOfCases(7));
+
+const otherSolution = (count) => {
+  let stairs = Array.from({ length: count }, () => 0);
+  stairs[1] = 1;
+  stairs[2] = 2;
+  for (let i = 3; i <= count; i += 1) {
+    stairs[i] = stairs[i - 1] + stairs[i - 2];
+  }
+
+  return stairs[count];
+};
