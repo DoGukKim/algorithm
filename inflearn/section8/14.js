0355e616ab303ccf40e5a46274e7a1be86c666b2
DoGukKim <dgkim3811@naver.com>
2022-07-30 08:58:18 +0900

    섹션 8 최종 풀이

--- inflearn/section8/14.js
+++ inflearn/section8/14.js
@@ -1,2 +1,21 @@
-const main = (numbers, m) => {};
+// Time: O(n^m)
+// Space: O(n*m)
+const main = (
+  numbers,
+  m,
+  temp = numbers.slice(),
+  index = 0,
+  level = 0,
+  allCombination = []
+) => {
+  if (level === m) return allCombination.push(temp.slice(0, 2));
+
+  for (let i = index; i < numbers.length; i++) {
+    temp[level] = i + 1;
+    main(numbers, m, temp, i + 1, level + 1, allCombination);
+  }
+
+  return allCombination;
+};
+
 console.log(main([1, 2, 3, 4], 2));
