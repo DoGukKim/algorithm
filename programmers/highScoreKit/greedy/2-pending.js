08492a34021584e317e95f70ca5c60656213c5b7
DoGukKim <dgkim3811@naver.com>
2022-08-17 18:43:20 +0900

    프로그래머스 고득점 kit 문제 풀이

---
+++ programmers/highScoreKit/greedy/2-pending.js
@@ -0,0 +1,18 @@
+// const main = (name) => {
+//   const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   let position = 0;
+
+//   for (let i = 0; i < name.length; i++) {
+//     position = alpha.indexOf(name[i]);
+//     dfs(name[i], position);
+//   }
+
+//   function dfs(target, pos) {
+//     if (target === alpha[pos]) {
+
+//   }
+// };
+
+// // console.log(main("JEROEN"));
+// // console.log(main("JAN"));
+// console.log(main("BE"));
--- programmers/highScoreKit/stack&queue/1.js
+++ programmers/highScoreKit/stack&queue/1.js
@@ -1,21 +1,17 @@
-// 기능개발
+// 같은 숫자는 싫어
 // Time: O(n)
 // Space: O(n)
-function solution(progresses, speeds) {
-  let answer = [0];
-  let days = progresses.map((progress, index) =>
-    Math.ceil((100 - progress) / speeds[index])
-  );
-  let maxDay = days[0];
+const main = (arr) => {
+  const stack = [];
 
-  for (let i = 0, j = 0; i < days.length; i++) {
-    if (days[i] <= maxDay) {
-      answer[j] += 1;
-    } else {
-      maxDay = days[i];
-      answer[++j] = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (stack[stack.length - 1] !== arr[i]) {
+      stack.push(arr[i]);
     }
   }
 
-  return answer;
-}
+  return stack;
+};
+
+console.log(main([1, 1, 3, 3, 0, 1, 1])); // -> 1,3,0,1
+console.log(main([4, 4, 4, 3, 3])); // -> 4,3
