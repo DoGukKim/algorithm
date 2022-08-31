b579c8933f6e49cf35ed34893967dd463102dbf3
DoGukKim <dgkim3811@naver.com>
2022-08-31 16:30:22 +0900

    재귀 복습

--- book/recursion/countX.js
+++ book/recursion/countX.js
@@ -1,9 +1,29 @@
-// TimeComplexity O(n^2)
-// SpaceComplexity O(n)
-const main = (string, count = 0) => {
-  if (string.length === 0) return count;
-  if (string[0] === "X") count += 1;
-  return main(string.slice(1), count);
+// 방법 1
+// Time: O(n)
+// Space: O(n)
+const main = (s) => {
+  let result = 0;
+
+  function dfs(index) {
+    if (index === s.length) return;
+
+    if (s[index] === "X") result++;
+    dfs(index + 1);
+  }
+  dfs(0);
+
+  return result;
 };
 
-console.log(main("XCX"));
+console.log(main("AXBXCX"));
+
+// 방법 2
+// Time: O(n^2)
+// Space: O(n)
+const main2 = (s) => {
+  if (s.length === 0) return 0;
+  const remainder = main2(s.slice(1));
+  return s[0] === "X" ? remainder + 1 : remainder + 0;
+};
+
+console.log(main2("AXBXCX"));
--- book/recursion/reverseString.js
+++ book/recursion/reverseString.js
@@ -1,8 +1,27 @@
-// Time: O(n^2)
-// Space: O(n)
+// 방법 1
+// Time : O(n)
+// Space : O(n)
 const main = (s) => {
-  if (s.length === 0) return "";
-  return main(s.slice(1)) + s[0]; // "doo" + "g"
+  let result = "";
+
+  function dfs(index) {
+    if (index < 0) return;
+    result += s[index];
+    dfs(index - 1);
+  }
+  dfs(s.length - 1);
+
+  return result;
 };
 
-console.log(main("good")); // -> "doog"
+console.log(main("abcd"));
+
+// 방법 2
+// Time : O(n^2)
+// Space : O(n)
+const main2 = (s) => {
+  if (s.length === 0) return "";
+  return main2(s.slice(1)) + s[0];
+};
+
+console.log(main2("abcd"));
