fbce6abb1d1303b49711c9d4dfb117d5a1f4edf4
DoGukKim <dgkim3811@naver.com>
2022-11-30 21:45:15 +0900

    백준 문제 풀이

---
+++ baekjoon/sliver/1697.js
@@ -0,0 +1,20 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+const [n, k] = input.split(" ").map(Number);
+const dist = Array.from({ length: 100002 }, () => -1);
+
+const queue = [n];
+dist[n] = 0;
+
+while (dist[k] === -1) {
+  const d = queue.shift();
+
+  for (const nx of [d - 1, d + 1, 2 * d]) {
+    if (dist[nx] !== -1) continue;
+    if (nx < 0 || nx > 100000) continue;
+    dist[nx] = dist[d] + 1;
+    queue.push(nx);
+  }
+}
+
+console.log(dist[k]);
