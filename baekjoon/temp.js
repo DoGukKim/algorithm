8eac2e8d3f4d74edcb90572a5589452e397f80bd
DoGukKim <dgkim3811@naver.com>
2022-12-04 21:54:22 +0900

    백준 문제 풀이

--- baekjoon/temp.js
+++ baekjoon/temp.js
@@ -1,11 +1,4 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().trim();
 // const input = require("fs").readFileSync("./input.txt").toString().trim();
-
-// const input = require("fs")
-//   .readFileSync("/dev/stdin")
-//   .toString()
-//   .trim()
-//   .split("\n");
 
 // const input = require("fs")
 //   .readFileSync("./input.txt")
