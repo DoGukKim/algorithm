d778d159b63012a95badd840260f075866529cac
DoGukKim <dgkim3811@naver.com>
2022-12-12 22:48:37 +0900

    백준 문제 풀이

---
+++ baekjoon/bronze/25501.js
@@ -0,0 +1,24 @@
+// Title: 재귀의 귀재
+// Time: O(n)
+// Space: O(n)
+let input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+input.shift();
+let result = "";
+
+for (let i = 0; i < input.length; i++) {
+  const [isPalindrome, cnt] = dfs(input[i]);
+  result += `${isPalindrome} ${cnt}\n`;
+}
+
+function dfs(s, lp = 0, rp = s.length - 1, cnt = 1) {
+  if (lp >= rp) return ["1", cnt];
+  if (s[lp] !== s[rp]) return ["0", cnt];
+  return dfs(s, lp + 1, rp - 1, cnt + 1);
+}
+
+console.log(result);
