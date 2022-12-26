90df1ad73c635a1e024a4ec79d5568fd62bbde16
DoGukKim <dgkim3811@naver.com>
2022-12-26 19:16:38 +0900

    백준 문제 풀이

---
+++ baekjoon/bronze/1037.js
@@ -0,0 +1,12 @@
+// Title: 약수
+// Time: O(1)
+// Space: O(1)
+let input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+input = input.map((i) => i.split(" ").map(Number));
+input.shift();
+input = input[0];
+console.log(Math.min(...input) * Math.max(...input));
