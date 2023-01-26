70ad93a873fd33cfd604780fefca3ed185df2209
DoGukKim <dgkim3811@naver.com>
2023-01-26 16:33:15 +0900

    백준 문제 풀이

--- baekjoon/gold/2206-pending.js
+++ baekjoon/gold/2206-pending.js
@@ -9,26 +9,29 @@ input = input.map((i) => i.split("").map(Number));
 const dx = [-1, 0, 1, 0];
 const dy = [0, 1, 0, -1];
 
-const queue = [[0, 0, 0, 1]];
+const queue = [[0, 0, 1, 1]];
 ch[0][0] = 1;
-while (queue.length) {
-  const [x, y, s, c] = queue.shift();
-  if (x === n - 1 && y === m - 1) return console.log(s + 1);
+let index = 0;
+while (queue.length > index) {
+  const [x, y, s, c] = queue[index++];
+  if (x === n - 1 && y === m - 1) return console.log(s);
 
   for (let i = 0; i < 4; i++) {
     const nx = x + dx[i];
     const ny = y + dy[i];
     if (0 <= nx && nx < n && 0 <= ny && ny < m && ch[nx][ny] === 0) {
-      if (input[nx][ny] === 1 && c === 1) {
-        ch[nx][ny] = 1;
-        queue.push([nx, ny, s + 1, c - 1]);
-        input[nx][ny] = 0;
-      } else if (input[nx][ny] === 0) {
+      if (c === 0 && input[nx][ny] === 1) continue;
+      if (input[nx][ny] === 0) {
         ch[nx][ny] = 1;
         queue.push([nx, ny, s + 1, c]);
+      } else if (c === 1 && input[nx][ny] === 1) {
+        ch[nx][ny] = 1;
+        queue.push([nx, ny, s + 1, c - 1]);
       }
     }
   }
+
+  queue.sort((a, b) => b[3] - a[3]);
 }
 
 console.log(-1);
---
+++ baekjoon/sliver/1325.js
@@ -0,0 +1,52 @@
+// Title: 효율적인 해킹
+// Time: O(v^2*e)
+// Space: O(v+e)
+let input = require("fs")
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+input = input.map((i) => i.split(" ").map(Number));
+const [n, m] = input[0];
+const h = Array.from({ length: n + 1 }, () => 0);
+let max = 0;
+const graph = {};
+for (let i = 1; i <= n; i++) {
+  graph[i] = [];
+}
+for (let i = 1; i <= m; i++) {
+  const [a, b] = input[i];
+  graph[b].push(a);
+}
+
+for (let i = 1; i <= n; i++) {
+  const ch = Array.from({ length: n + 1 }, () => 0);
+  ch[i] = 1;
+  const cnt = dfs(i, ch);
+  h[i] = cnt;
+  if (max < cnt) max = cnt;
+}
+
+let result = "";
+for (let i = 0; i < h.length; i++) {
+  if (max === h[i]) result += `${i} `;
+}
+console.log(result);
+
+function dfs(v, ch) {
+  const stack = [v];
+  let cnt = 0;
+
+  while (stack.length) {
+    const c = stack.pop();
+    for (let i = 0; i < graph[c].length; i++) {
+      if (ch[graph[c][i]] === 0) {
+        cnt++;
+        ch[graph[c][i]] = 1;
+        stack.push(graph[c][i]);
+      }
+    }
+  }
+
+  return cnt;
+}
