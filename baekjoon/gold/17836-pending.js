c5a932a94d9ff6242d6498808861c6d5e47af208
DoGukKim <dgkim3811@naver.com>
2023-02-08 21:14:39 +0900

    백준 문제 풀이

--- baekjoon/gold/17836-pending.js
+++ baekjoon/gold/17836-pending.js
@@ -1,52 +1,49 @@
-let input = require("fs")
-  .readFileSync(__dirname + "/input.txt")
-  .toString()
-  .trim()
-  .split("\n");
-const [n, m, t] = input.shift().split(" ").map(Number);
-input = input.map((i) => i.split(" ").map(Number));
-const ch = Array.from({ length: n }, () => Array(m).fill(0));
-const dx = [-1, 0, 1, 0];
-const dy = [0, 1, 0, -1];
+// let input = require("fs")
+//   .readFileSync(__dirname + "/input.txt")
+//   .toString()
+//   .trim()
+//   .split("\n");
+// input = input.map((i) => i.split(" ").map(Number));
+// const [n, m, time] = input.shift();
+// const dir = [
+//   [-1, 0],
+//   [0, 1],
+//   [1, 0],
+//   [0, -1],
+// ];
 
-let queue = [[0, 0, 0, false, t]];
-if (input[0][0] === 2) queue = [[0, 0, 0, true, t]];
-ch[0][0] = 1;
-let index = 0;
+// let queue = [];
+// if (input[0][0] === 2) queue = [[0, 0, 0, true]];
+// else queue = [[0, 0, 0, false]];
+// input[0][0] = 3;
 
-while (queue.length > index) {
-  const [x, y, dis, has, time] = queue[index++];
-  if (x === n - 1 && y === m - 1) return console.log(dis);
+// while (queue.length) {
+//   const [x, y, t, isHasGram] = queue.shift();
+//   if (time - t === 0) return console.log("Fail");
+//   if (x === n - 1 && y === m - 1) return console.log(t);
 
-  for (let i = 0; i < 4; i++) {
-    const nx = x + dx[i];
-    const ny = y + dy[i];
-    if (
-      0 <= nx &&
-      nx < n &&
-      0 <= ny &&
-      ny < m &&
-      ch[nx][ny] === 0 &&
-      time - 1 > 0
-    ) {
-      if (has) {
-        ch[nx][ny] = 1;
-        queue.push([nx, ny, dis + 1, has, time - 1]);
-        continue;
-      }
+//   for (let i = 0; i < 4; i++) {
+//     const [dx, dy] = dir[i];
+//     const nx = x + dx;
+//     const ny = y + dy;
+//     if (0 <= nx && nx < n && 0 <= ny && ny < m && input[nx][ny] !== 3) {
+//       if (input[nx][ny] === 2) {
+//         console.log(time - (t + 1));
+//         input[nx][ny] = 3;
+//         queue.push([nx, ny, t + 1, true]);
+//         continue;
+//       }
 
-      if (input[nx][ny] === 2) {
-        ch[nx][ny] = 1;
-        queue.push([nx, ny, dis + 1, true, time - 1]);
-        continue;
-      }
+//       if (isHasGram) {
+//         input[nx][ny] = 3;
+//         queue.push([nx, ny, t + 1, isHasGram]);
+//         continue;
+//       }
 
-      if (input[nx][ny] === 0) {
-        ch[nx][ny] = 1;
-        queue.push([nx, ny, dis + 1, has, time - 1]);
-      }
-    }
-  }
-}
-
-console.log("Fail");
+//       if (!isHasGram && input[nx][ny] === 0) {
+//         input[nx][ny] = 3;
+//         queue.push([nx, ny, t + 1, isHasGram]);
+//       }
+//     }
+//   }
+// }
