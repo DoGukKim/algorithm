e0a5069f1eb2287697500afdbc08a2ba93f82b0d
DoGukKim <dgkim3811@naver.com>
2023-02-19 16:27:03 +0900

    백준 문제 풀이

--- baekjoon/sliver/16926-pending.js
+++ baekjoon/sliver/16926-pending.js
@@ -3,73 +3,41 @@ let input = require("fs")
   .toString()
   .trim()
   .split("\n");
+const [n, m, r] = input.shift().split(" ").map(Number);
 input = input.map((i) => i.split(" ").map(Number));
-// const [n, m, r] = input.shift();
 
-dfs(0, m - 1, n, m);
-function dfs(x, y, n, m) {
-  if (n < 2 || m < 2) return;
-  handleRotate(x, y);
-  //   dfs(x + 1, y - 1, n - 2, m - 2);
+for (let i = 0; i < r; i++) {
+  rotate();
 }
 
-function handleRotate(x, y) {
-  let prev = 0;
+let result = "";
+for (let i = 0; i < n; i++) {
+  result += `${input[i].join(" ")}\n`;
+}
+console.log(result);
 
-  for (let i = y; i >= x; i--) {
-    let temp = input[x][i];
-    input[x][i] = prev;
-    prev = temp;
+function rotate() {
+  const rotateCountOfInside = Math.min(n, m) / 2;
+
+  for (let cnt = 0; cnt < rotateCountOfInside; cnt++) {
+    let temp = input[cnt][cnt];
+
+    for (let i = cnt; i < m - cnt - 1; i++) {
+      input[cnt][i] = input[cnt][i + 1];
+    }
+
+    for (let i = cnt; i < n - cnt - 1; i++) {
+      input[i][m - cnt - 1] = input[i + 1][m - cnt - 1];
+    }
+
+    for (let i = m - cnt - 1; i > cnt; i--) {
+      input[n - cnt - 1][i] = input[n - cnt - 1][i - 1];
+    }
+
+    for (let i = n - cnt - 1; i > cnt; i--) {
+      input[i][cnt] = input[i - 1][cnt];
+    }
+
+    input[cnt + 1][cnt] = temp;
   }
-
-  for (let i = x + 1; i < n - x; i++) {
-    let temp = input[i][x];
-    input[i][x] = prev;
-    prev = temp;
-  }
-
-  for (let i = x + 1; i <= y; i++) {
-    let temp = input[n - 1 - x][i];
-    input[n - 1 - x][i] = prev;
-    prev = temp;
-  }
-
-  for (let i = n - 1 - x; i >= x; i++) {}
-
-  console.log(prev);
-  console.log(input);
 }
-
-const arr = [
-  [1, 2],
-  [3, 4],
-];
-const n = 2;
-const m = 2;
-let prev = 0;
-
-for (let i = n - 1; i >= 0; i--) {
-  let temp = arr[0][i];
-  arr[0][i] = prev;
-  prev = temp;
-}
-
-for (let i = 1; i < n; i++) {
-  let temp = arr[i][0];
-  arr[i][0] = prev;
-  prev = temp;
-}
-
-for (let i = 1; i < m; i++) {
-  let temp = arr[n - 1][i];
-  arr[n - 1][i] = prev;
-  prev = temp;
-}
-
-for (let i = n - 2; i >= 0; i--) {
-  let temp = arr[i][m - 1];
-  arr[i][m - 1] = prev;
-  prev = temp;
-}
-
-console.log(prev, arr);
