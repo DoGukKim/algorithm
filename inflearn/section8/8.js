700560778c47d2f04f31e936b297ee3590b7631a
DoGukKim <dgkim3811@naver.com>
2023-02-24 20:59:08 +0900

    인프런 복습 및 시간 복잡도, 공간 복잡도 수정

--- inflearn/section8/8.js
+++ inflearn/section8/8.js
@@ -1,52 +1,25 @@
 // Title: 중복순열 구하기
-// 방법 1
 // Time: O(n^n)
 // Space: O(m)
 function main(n, m) {
-  let result = 0;
-
-  function dfs(p) {
-    if (p.length === m) {
-      console.log(p.join(" "));
-      result++;
-      return;
-    }
-
-    for (let i = 1; i <= n; i++) {
-      p.push(i);
-      dfs(p);
-      p.pop();
-    }
-  }
-
-  dfs([]);
-  console.log(result);
-}
-
-main(3, 2);
-
-// 방법 2
-// Time: O(n^n)
-// Space: O(m)
-function main(n, m) {
-  const p = Array.from({ length: m }, () => 0);
-  let result = 0;
-
-  function dfs(l) {
-    if (l === m) {
-      console.log(p.join(" "));
-      result++;
-      return;
-    }
-
-    for (let i = 1; i <= n; i++) {
-      p[l] = i;
-      dfs(l + 1);
-    }
-  }
-
+  const permute = Array.from({ length: m }, () => 0);
+  let count = 0;
   dfs(0);
-  console.log(result);
+  console.log(count);
+
+  function dfs(level) {
+    if (level === m) {
+      count++;
+      console.log(permute);
+      return;
+    }
+
+    for (let i = 1; i <= n; i++) {
+      permute[level] = i;
+      dfs(level + 1);
+      permute[level] = 0;
+    }
+  }
 }
 
 main(3, 2);
--- inflearn/section8/9.js
+++ inflearn/section8/9.js
@@ -1,21 +1,20 @@
 // Title: 동전교환
 // Time: O(n^n)
 // Space: O(min(m))
-function main(n, c, m) {
+function main(n, coins, m) {
   let result = Infinity;
-
-  function dfs(sum, cnt) {
-    if (sum === m) return cnt;
-
-    for (let i = 0; i < n; i++) {
-      if (sum + c[i] > m) continue;
-      const min = dfs(sum + c[i], cnt + 1);
-      if (min < result) result = min;
-    }
-  }
-
   dfs(0, 0);
   console.log(result);
-}
 
-main(3, [1, 2, 5], 15);
+  function dfs(count, sum) {
+    if (sum > m) return;
+    if (sum === m) {
+      if (count < result) result = count;
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      dfs(count + 1, sum + coins[i]);
+    }
+  }
+}
