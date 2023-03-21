4c9e8ddf6f0b4056e52f329b4622ae671b961e34
DoGukKim <dgkim3811@naver.com>
2023-03-22 01:01:45 +0900

    인프런 복습 및 시간 복잡도, 공간 복잡도 수정

--- inflearn/section5/7.js
+++ inflearn/section5/7.js
@@ -6,15 +6,7 @@ function main(s1, s2) {
   s1 = s1.split("").sort();
   s2 = s2.split("").sort();
 
-  for (let i = 0; i < s1.length; i++) {
-    if (s1[i] !== s2[i]) {
-      console.log("NO");
-      return;
-    }
-  }
-
-  console.log("YES");
-  // or console.log(s1.join("") === s2.join(""));
+  console.log(s1.join("") === s2.join(""));
 }
 
 // 방법 2
@@ -23,13 +15,9 @@ function main(s1, s2) {
 function main(s1, s2) {
   const map = new Map();
 
-  for (let i = 0; i < s1.length; i++) {
-    map.set(s1[i], (map.get(s1[i]) || 0) + 1);
-  }
+  for (let i = 0; i < s1.length; i++) map.set(s1[i], (map.get(s1[i]) || 0) + 1);
 
-  for (let i = 0; i < s2.length; i++) {
-    map.set(s2[i], map.get(s2[i]) - 1);
-  }
+  for (let i = 0; i < s2.length; i++) map.set(s2[i], map.get(s2[i]) - 1);
 
   for (const [_, v] of map) {
     if (v !== 0) {
@@ -37,6 +25,7 @@ function main(s1, s2) {
       return;
     }
   }
+
   console.log("YES");
 }
 
--- inflearn/section5/8.js
+++ inflearn/section5/8.js
@@ -1,51 +1,72 @@
 // Title: 아나그램(2)
 // 방법 1
-// Time: O(n!*t)
-// Space: O(t!)
+// Time: O(s*t)
+// Space: O(t)
 function main(s, t) {
-  const allPermutation = [];
+  const sHash = new Map();
+  const tHash = new Map();
 
-  function dfs(p) {
-    if (t.length === p.length) {
-      allPermutation.push(p.join(""));
-      return;
-    }
-
-    for (let i = 0; i < t.length; i++) {
-      if (p.includes(t[i])) continue;
-      p.push(t[i]);
-      dfs(p);
-      p.pop();
-    }
+  for (let i = 0; i < t.length; i++) {
+    tHash.set(t[i], (tHash.get(t[i]) || 0) + 1);
   }
-  dfs([]);
 
+  let lt = 0;
   let result = 0;
-  for (let i = 0; i < allPermutation.length; i++) {
-    if (s.includes(allPermutation[i])) result++;
+  for (let rt = 0; rt < s.length; rt++) {
+    sHash.set(s[rt], (sHash.get(s[rt]) || 0) + 1);
+
+    while (rt - lt > t.length - 1) {
+      sHash.set(s[lt], sHash.get(s[lt]) - 1);
+      if (sHash.get(s[lt]) === 0) {
+        sHash.delete(s[lt]);
+      }
+      lt++;
+    }
+
+    if (compareMap(sHash, tHash)) result++;
   }
 
   console.log(result);
+
+  function compareMap(sm, tm) {
+    if (sm.size !== tm.size) return false;
+    for (const [k, v] of sm) {
+      if (!tm.has(k) || tm.get(k) !== v) return false;
+    }
+
+    return true;
+  }
 }
 
 // 방법 2
-// Time: O(s*t)
-// Space: O(1)
+// Time: O(s)
+// Space: O(t)
 function main(s, t) {
+  let hash = new Map();
+
+  for (let i = 0; i < t.length; i++) {
+    hash.set(t[i], (hash.get(t[i]) || 0) + 1);
+  }
+
+  for (let i = 0; i < t.length - 1; i++) {
+    hash.set(s[i], (hash.get(s[i]) || 0) - 1);
+    if (hash.get(s[i]) === 0) hash.delete(s[i]);
+  }
+
+  let lt = 0;
   let result = 0;
+  for (let rt = t.length - 1; rt < s.length; rt++) {
+    hash.set(s[rt], (hash.get(s[rt]) || 0) - 1);
+    if (hash.get(s[rt]) === 0) hash.delete(s[rt]);
+    if (hash.size === 0) result++;
 
-  for (let i = 0; i < s.length; i++) {
-    let sum = "";
-    let pointer = i;
-    let count = t.length;
-
-    while (count--) {
-      sum += s[pointer++];
-    }
-
-    const sorted = sum.split("").sort().join("");
-    if (sorted === t) result++;
+    hash.set(s[lt], (hash.get(s[lt]) || 0) + 1);
+    if (hash.get(s[lt]) === 0) hash.delete(s[lt]);
+    if (hash.size === 0) result++;
+    lt++;
   }
 
   console.log(result);
 }
+
+main("bacaAacba", "abc");
