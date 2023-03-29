9ae68e639abd6d1601ac68bc71d3ec44cf4f602c
DoGukKim <dgkim3811@naver.com>
2023-03-29 10:55:07 +0900

    인프런 복습 및 시간 복잡도, 공간 복잡도 수정

--- inflearn/section7/11.js
+++ inflearn/section7/11.js
@@ -1,42 +1,41 @@
 // Title: 뮤직 비디오
-// Time: O(log n)
+// Math.max, reduce에서 O(n)으로 동작하기 떄문에 log n으로 표현할 수 없습니다.
+// Time: O(n)
 // Space: O(1)
-const main = (n, m) => {
-  let lp = n;
-  let rp = (n + 1) * (n / 2);
+function main(n, m, songs) {
   let result = 0;
+  let lt = Math.max(...songs);
+  let rt = songs.reduce((a, b) => a + b, 0);
 
-  while (lp <= rp) {
-    const mid = Math.floor((lp + rp) / 2);
-    let sum = 0;
-    let dvd = 1; // 1장 부터 시작
+  while (lt <= rt) {
+    const pivot = Math.floor((lt + rt) / 2);
 
-    for (let i = 1; i <= n; i++) {
-      if (sum + i > mid) {
-        // 누적 분량이 dvd 1장의 공간 보다 커지면 dvd 1장 추가하고
-        // sum은 현재 노래로 재할당
-        dvd++;
-        sum = i;
-      } else {
-        sum += i;
-      }
-    }
-
-    if (dvd <= m) {
-      // 장수가 m 보다 작거나 같으면 여유가 있는 분량으로 줄여나가면서 다시 이분 검색
-      result = mid;
-      rp = mid - 1;
+    if (countDVD(pivot) <= m) {
+      result = pivot;
+      rt = pivot - 1;
     } else {
-      // 여유가 없으니 분량을 증가시켜 다시 이분 검색
-      lp = mid + 1;
+      lt = pivot + 1;
     }
   }
 
   console.log(result);
-};
 
-main(9, 3);
+  function countDVD(capacity) {
+    let cnt = 1;
+    let sum = 0;
 
-function main(n, m, songs) {}
+    for (let i = 0; i < n; i++) {
+      if (sum + songs[i] > capacity) {
+        cnt++;
+        sum = songs[i];
+        continue;
+      }
+
+      sum += songs[i];
+    }
+
+    return cnt;
+  }
+}
 
 main(9, 3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
--- inflearn/section7/12.js
+++ inflearn/section7/12.js
@@ -1,39 +1,39 @@
 // Title: 마구간 정하기
 // Time: O(n log n)
 // Space: O(1)
-const main = (n, m) => {
-  n.sort((a, b) => a - b);
+function main(n, c, coordinates) {
+  coordinates.sort((a, b) => a - b);
 
-  let lp = 1; // 1인 이유는 마구간의 좌표가 1부터이기 때문에.
-  let rp = n[n.length - 1];
+  let lt = 1;
+  let rt = coordinates[coordinates.length - 1];
   let result = 0;
 
-  while (lp <= rp) {
-    const mid = Math.floor((lp + rp) / 2); // 마구간 간의 최소한의 거리
-    let horse = 1; // 말 하나를 시작점에 놓고
-    let recentPos = n[0]; // 말 시작점 초기 값 할당
+  while (lt <= rt) {
+    const pivot = Math.floor((lt + rt) / 2);
 
-    for (let i = 1; i < n.length; i++) {
-      if (n[i] - recentPos >= mid) {
-        // 현재 마구간의 거리와 지난 마구간 위치를 뺏을 때 최소한의 거리와 같거나 크면 말 넣어주고
-        // 지난 위치 재할당
-        horse++;
-        recentPos = n[i];
-      }
-    }
-
-    if (horse >= m) {
-      // 넣어야 하는 말 만큼 넣었으면 최소한의 거리 삽입
-      // 더 큰 최소거리 값을 찾기 위해 다시 이분검색
-      result = mid;
-      lp = mid + 1;
+    if (countHorse(pivot) >= c) {
+      result = pivot;
+      lt = pivot + 1;
     } else {
-      // 말이 다 안들어 갔기 때문에 더 큰 범위 제거
-      rp = mid - 1;
+      rt = pivot - 1;
     }
   }
 
   console.log(result);
-};
 
-main([1, 2, 8, 4, 9], 3);
+  function countHorse(dist) {
+    let horse = 1;
+    let recent = coordinates[0];
+
+    for (let i = 1; i < n; i++) {
+      if (coordinates[i] - recent >= dist) {
+        horse++;
+        recent = coordinates[i];
+      }
+    }
+
+    return horse;
+  }
+}
+
+main(5, 3, [1, 2, 8, 4, 9]);
