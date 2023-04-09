0303c1062a09b1cdb39c11fab3bf103d465950d1
DoGukKim <dgkim3811@naver.com>
2023-04-09 14:02:50 +0900

    백준 문제 풀이

--- baekjoon/gold/17836.js
+++ baekjoon/gold/17836.js
@@ -8,121 +8,48 @@ let input = require("fs")
   .split("\n");
 input = input.map((i) => i.split(" ").map(Number));
 const [n, m, time] = input.shift();
-const dir = [
+const direction = [
   [-1, 0],
   [0, 1],
   [1, 0],
   [0, -1],
 ];
-
-const q1 = [[0, 0, 0]];
-let q2 = [];
-if (input[0][0] === 2) {
-  q2 = [[0, 0, 0]];
-  ch[0][0] = 1;
-}
-const ch = Array.from({ length: n }, () => Array(m).fill(0));
-
+const queue = [[0, 0, 0]];
+let gram = [];
+if (input[0][0] === 2) gram.push([0, 0, 0]);
 input[0][0] = 1;
-let a = "Fail";
-let b = "Fail";
-
-while (q1.length) {
-  const [x, y, t] = q1.shift();
-  if (x === n - 1 && y === m - 1) {
-    if (time - t >= 0) a = t;
-    break;
-  }
-
-  for (let i = 0; i < 4; i++) {
-    const [dx, dy] = dir[i];
-    const nx = x + dx;
-    const ny = y + dy;
-    if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-      if (input[nx][ny] === 2) q2.push([nx, ny, t + 1]);
-      if (input[nx][ny] === 1) continue;
-      input[nx][ny] = 1;
-      q1.push([nx, ny, t + 1]);
-    }
-  }
+let a = Infinity;
+let b = Infinity;
+bfs();
+if (gram.length) {
+  const [x, y, t] = gram[0];
+  const result = Math.abs(n - 1 - x) + Math.abs(m - 1 - y) + t;
+  if (result >= 0 && time >= result) b = result;
 }
 
-while (q2.length) {
-  const [x, y, t] = q2.shift();
-  if (x === n - 1 && y === m - 1) {
-    if (time - t >= 0) b = t;
-    break;
-  }
-
-  for (let i = 0; i < 4; i++) {
-    const [dx, dy] = dir[i];
-    const nx = x + dx;
-    const ny = y + dy;
-    if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-      if (ch[nx][ny] === 1) continue;
-      ch[nx][ny] = 1;
-      q2.push([nx, ny, t + 1]);
-    }
-  }
-}
-
-const result = Math.min(
-  a !== "Fail" ? a : Infinity,
-  b !== "Fail" ? b : Infinity
-);
+const result = Math.min(a, b);
 console.log(result !== Infinity ? result : "Fail");
 
-// 한 번의 BFS 수행으로 한 경우
-// let input = require("fs")
-//   .readFileSync(__dirname + "/input.txt")
-//   .toString()
-//   .trim()
-//   .split("\n");
-// input = input.map((i) => i.split(" ").map(Number));
-// const [n, m, time] = input.shift();
-// const dir = [
-//   [-1, 0],
-//   [0, 1],
-//   [1, 0],
-//   [0, -1],
-// ];
+function bfs() {
+  while (queue.length) {
+    const [x, y, t] = queue.shift();
+    if (x === n - 1 && y === m - 1) {
+      if (time - t >= 0) a = t;
+      break;
+    }
 
-// const q1 = [[0, 0, 0]];
-// let q2 = [];
-// if (input[0][0] === 2) q2 = [[0, 0, 0]];
+    for (let i = 0; i < 4; i++) {
+      const [dx, dy] = direction[i];
+      const nx = x + dx;
+      const ny = y + dy;
 
-// input[0][0] = 1;
-// let a = "Fail";
-// let b = "Fail";
+      if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+        if (input[nx][ny] === 2) gram.push([nx, ny, t + 1]);
+        if (input[nx][ny] === 1) continue;
 
-// while (q1.length) {
-//   const [x, y, t] = q1.shift();
-//   if (x === n - 1 && y === m - 1) {
-//     if (time - t >= 0) a = t;
-//     break;
-//   }
-
-//   for (let i = 0; i < 4; i++) {
-//     const [dx, dy] = dir[i];
-//     const nx = x + dx;
-//     const ny = y + dy;
-//     if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-//       if (input[nx][ny] === 2) q2.push([nx, ny, t + 1]);
-//       if (input[nx][ny] === 1) continue;
-//       input[nx][ny] = 1;
-//       q1.push([nx, ny, t + 1]);
-//     }
-//   }
-// }
-
-// if (q2.length) {
-//   const [x, y, t] = q2.shift();
-//   const result = Math.abs(n - 1 - x) + Math.abs(m - 1 - y) + t;
-//   if (result >= 0 && time <= result) b = result;
-// }
-
-// const result = Math.min(
-//   a !== "Fail" ? a : Infinity,
-//   b !== "Fail" ? b : Infinity
-// );
-// console.log(result !== Infinity ? result : "Fail");
+        input[nx][ny] = 1;
+        queue.push([nx, ny, t + 1]);
+      }
+    }
+  }
+}
--- baekjoon/gold/2573.js
+++ baekjoon/gold/2573.js
@@ -1,75 +1,94 @@
 // Title: 빙산
-// Time: O(nm*v+e)
-// Space: O(nm*v+e)
+// Time: O(nm)
+// Space: O(nm)
 let input = require("fs")
   .readFileSync(__dirname + "/input.txt")
   .toString()
   .trim()
   .split("\n");
-const [n, m] = input.shift().split(" ").map(Number);
 input = input.map((i) => i.split(" ").map(Number));
-const dx = [-1, 0, 1, 0];
-const dy = [0, 1, 0, -1];
+const [n, m] = input.shift();
+const direction = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
 let result = 0;
-
 while (true) {
-  bfs();
+  const copy = Array.from({ length: n }, () => Array(m).fill(0));
+  for (let x = 0; x < n; x++) {
+    for (let y = 0; y < m; y++) {
+      copy[x][y] = input[x][y];
+    }
+  }
 
-  let cnt = 0;
+  melt(input, copy);
+  input = copy;
+
+  let count = 0;
   const ch = Array.from({ length: n }, () => Array(m).fill(0));
   for (let x = 1; x < n - 1; x++) {
     for (let y = 1; y < m - 1; y++) {
       if (input[x][y] > 0 && ch[x][y] === 0) {
-        dfs(x, y, ch);
-        cnt++;
+        ch[x][y] = 1;
+        dfs(x, y, input, ch);
+        count++;
       }
     }
   }
 
   result++;
-  if (cnt > 1) return console.log(result);
-  if (cnt === 0) return console.log(0);
+  if (count === 0) return console.log(0);
+  if (count >= 2) return console.log(result);
 }
 
-function bfs() {
-  const mx = Array.from({ length: n }, () => Array(m).fill(0));
-  const queue = [];
-  for (let x = 1; x < n - 1; x++) {
-    for (let y = 1; y < m - 1; y++) {
-      mx[x][y] = input[x][y];
-      if (input[x][y] > 0) queue.push([x, y]);
-    }
-  }
+function dfs(x, y, input, ch) {
+  const stack = [[x, y]];
 
-  while (queue.length) {
-    const [x, y] = queue.shift();
+  while (stack.length) {
+    const [x, y] = stack.pop();
 
     for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-      if (input[x][y] === 0) continue;
-      if (0 <= nx && nx < n && 0 <= ny && ny < m && mx[nx][ny] === 0) {
-        input[x][y] -= 1;
+      const [dx, dy] = direction[i];
+      const nx = x + dx;
+      const ny = y + dy;
+
+      if (
+        0 <= nx &&
+        nx < n &&
+        0 <= ny &&
+        ny < m &&
+        input[nx][ny] > 0 &&
+        ch[nx][ny] === 0
+      ) {
+        ch[nx][ny] = 1;
+        stack.push([nx, ny]);
       }
     }
   }
 }
 
-function dfs(x, y, ch) {
-  ch[x][y] = 1;
+function melt(input, copy) {
+  for (let x = 1; x < n - 1; x++) {
+    for (let y = 1; y < m - 1; y++) {
+      if (input[x][y] === 0) continue;
 
-  for (let i = 0; i < 4; i++) {
-    const nx = x + dx[i];
-    const ny = y + dy[i];
-    if (
-      0 <= nx &&
-      nx < n &&
-      0 <= ny &&
-      ny < m &&
-      input[nx][ny] > 0 &&
-      ch[nx][ny] === 0
-    ) {
-      dfs(nx, ny, ch);
+      for (let i = 0; i < 4; i++) {
+        const [dx, dy] = direction[i];
+        const nx = x + dx;
+        const ny = y + dy;
+        if (
+          0 <= nx &&
+          nx < n &&
+          0 <= ny &&
+          ny < m &&
+          input[nx][ny] === 0 &&
+          copy[x][y] > 0
+        ) {
+          copy[x][y] -= 1;
+        }
+      }
     }
   }
 }
