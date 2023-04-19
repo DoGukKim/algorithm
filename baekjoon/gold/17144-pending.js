f88355eb24bf81f4ab222ce66dfe253c995ebef2
DoGukKim <dgkim3811@naver.com>
2023-04-19 16:44:50 +0900

    백준 문제 풀이

--- baekjoon/gold/17144-pending.js
+++ baekjoon/gold/17144-pending.js
@@ -3,9 +3,11 @@ let input = require("fs")
   .toString()
   .trim()
   .split("\n");
+// 시간 초과로 실패
 input = input.map((i) => i.split(" ").map(Number));
 const [R, C, T] = input.shift();
-const airCleanerPosition = [0, 0];
+const cleanerPos = { top: 0, bottom: 0 };
+const add = Array.from({ length: R }, () => Array(C).fill(0));
 const direction = [
   [-1, 0],
   [0, 1],
@@ -15,61 +17,138 @@ const direction = [
 
 for (let x = 1; x < R; x++) {
   if (input[x][0] === -1) {
-    airCleanerPosition[0] = x;
-    airCleanerPosition[1] = x + 1;
+    cleanerPos["top"] = x;
+    cleanerPos["bottom"] = x + 1;
     break;
   }
 }
 
-console.log(airCleanerPosition);
-
 for (let i = 0; i < T; i++) {
-  const dust = [];
-  for (let x = 0; x < R; x++) {
-    for (let y = 0; y < C; y++) {
-      if (input[x][y] > 0) dust.push([x, y]);
-    }
-  }
-
-  diffusion(dust);
-  cleanAir("top");
-  //   cleanAir("bottom");
-
-  return;
+  dustDiffusion();
+  cleaningAirBy("top");
+  cleaningAirBy("bottom");
 }
 
-let result = 0;
+const result = calcRemainDust();
+console.log(result);
 
-function diffusion(dust) {
-  while (dust.length) {
-    let count = 0;
-    const [x, y] = dust.shift();
+function dustDiffusion() {
+  const room = Array.from({ length: R }, () => Array(C).fill(0));
+
+  for (let x = 0; x < R; x++) {
+    for (let y = 0; y < C; y++) {
+      if (input[x][y] === -1 || input[x][y] === 0) continue;
+      diffusion(x, y);
+      updateDustStatus();
+    }
+  }
+  // 다음 순환을 위한 현재 상태 재할당
+  input = room;
+  input[cleanerPos["top"]][0] = -1;
+  input[cleanerPos["bottom"]][0] = -1;
+
+  function diffusion(x, y) {
     const amount = Math.floor(input[x][y] / 5);
+    let count = 0;
 
     for (let i = 0; i < 4; i++) {
       const [dx, dy] = direction[i];
       const nx = x + dx;
       const ny = y + dy;
+
       if (0 <= nx && nx < R && 0 <= ny && ny < C && input[nx][ny] !== -1) {
-        input[nx][ny] += amount;
+        add[nx][ny] += amount;
         count++;
       }
     }
 
-    input[x][y] = input[x][y] - amount * count;
+    add[x][y] = input[x][y] - amount * count;
+  }
+
+  function updateDustStatus() {
+    for (let x = 0; x < R; x++) {
+      for (let y = 0; y < C; y++) {
+        room[x][y] += add[x][y];
+        add[x][y] = 0;
+      }
+    }
   }
 }
 
-function cleanAir(direction) {
+function cleaningAirBy(direction) {
   if (direction === "top") rotateTop();
-  //   if (direction === "bottom") rotateTop();
+  if (direction === "bottom") rotateBottom();
 }
 
 function rotateTop() {
   let temp = 0;
-  for (let i = 1; i < y; i++) {
-    const cur = mx[x][i];
-    mx[x][i] = temp;
-    temp = cur;
+  // right
+  for (let y = 1; y < C; y++) {
+    const subTemp = input[cleanerPos["top"]][y];
+    input[cleanerPos["top"]][y] = temp;
+    temp = subTemp;
   }
+  // up
+  for (let x = cleanerPos["top"] - 1; x >= 0; x--) {
+    const subTemp = input[x][C - 1];
+    input[x][C - 1] = temp;
+    temp = subTemp;
+  }
+  // left
+  for (let y = C - 2; y >= 0; y--) {
+    const subTemp = input[0][y];
+    input[0][y] = temp;
+    temp = subTemp;
+  }
+  // down
+  for (let x = 1; x <= cleanerPos["top"]; x++) {
+    if (input[x][0] === -1) continue;
+    const subTemp = input[x][0];
+    input[x][0] = temp;
+    temp = subTemp;
+  }
+}
+
+function rotateBottom() {
+  let temp = 0;
+  // right
+  for (let y = 1; y < C; y++) {
+    const subTemp = input[cleanerPos["bottom"]][y];
+    input[cleanerPos["bottom"]][y] = temp;
+    temp = subTemp;
+  }
+
+  // down
+  for (let x = cleanerPos["bottom"] + 1; x < R; x++) {
+    const subTemp = input[x][C - 1];
+    input[x][C - 1] = temp;
+    temp = subTemp;
+  }
+
+  // left
+  for (let y = C - 2; y >= 0; y--) {
+    const subTemp = input[R - 1][y];
+    input[R - 1][y] = temp;
+    temp = subTemp;
+  }
+
+  // top
+  for (let x = R - 2; x >= cleanerPos["bottom"]; x--) {
+    if (input[x][0] === -1) continue;
+    const subTemp = input[x][0];
+    input[x][0] = temp;
+    temp = subTemp;
+  }
+}
+
+function calcRemainDust() {
+  let count = 0;
+  for (let x = 0; x < R; x++) {
+    for (let y = 0; y < C; y++) {
+      if (input[x][y] === -1 || input[x][y] === 0) continue;
+      count += input[x][y];
+    }
+  }
+
+  return count;
 }
