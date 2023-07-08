65fd974d27ef9181f7c79ac9e49f048625d9fb8e
DoGukKim <dgkim3811@naver.com>
2023-07-08 12:54:49 +0900

    알고리즘 투 포인터 문제 풀이

--- baekjoon/sliver/21921.js
+++ baekjoon/sliver/21921.js
@@ -1,3 +1,6 @@
+// title: 블로그
+// time: O(n)
+// space: O(n)
 let input = require("fs")
   .readFileSync(__dirname + "/input.txt")
   .toString()
@@ -14,20 +17,18 @@ let rt = 0;
 let sum = 0;
 let max = 0;
 
-while (lt < n - x) {
-  if (Math.abs(lt - rt) === x) {
-    sum += input[rt];
-    map.set(sum, (map.get(sum) || 0) + 1);
-    max = Math.max(max, sum);
-    sum -= input[lt];
-    lt++;
+while (lt <= n - x) {
+  while (rt - lt < x) {
+    sum += input[rt++];
   }
 
-  sum += input[rt++];
+  map.set(sum, (map.get(sum) || 0) + 1);
+  max = Math.max(max, sum);
+  sum -= input[lt];
+  lt++;
 }
 
 if (max === 0) return console.log("SAD");
 
-console.log(map);
 console.log(max);
 console.log(map.get(max));
