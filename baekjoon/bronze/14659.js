850246b3fbb84a8a02ff6f8817350209f83cf79b
DoGukKim <dgkim3811@naver.com>
2023-10-16 20:18:30 +0900

    백준 그리디 브론즈 랜덤 디펜스

---
+++ baekjoon/bronze/14659.js
@@ -0,0 +1,40 @@
+// title: 한조서열정리하고옴ㅋㅋ
+// time: O(n)
+// space: O(nm)
+let input = require("fs")
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+const n = Number(input[0]);
+input = input[1].split(" ").map(Number);
+
+let queue = [];
+let result = 0;
+for (let i = 0; i < n; i++) {
+  if (queue.length === 0) {
+    queue.push(input[i]);
+    continue;
+  } else {
+    if (queue[0] > input[i]) {
+      queue.push(input[i]);
+      if (queue.length > 1 && i === n - 1) {
+        result = Math.max(result, queue.length - 1);
+      }
+      continue;
+    }
+
+    if (queue[0] < input[i]) {
+      result = Math.max(result, queue.length - 1);
+      queue = [input[i]];
+      continue;
+    }
+  }
+}
+
+console.log(result);
+
+// testCase
+// [1,2,3,4,5]
+// [5,4,3,2,1]
+// [1]
